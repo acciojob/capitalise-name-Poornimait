a417f5c894fa67188a7f3b5d60ff7d1e8fa84b7a
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-11-24 21:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+            // Function to convert input value to uppercase
+            function convertToUppercase() {
+                var inputField = document.getElementById('fname');
+                inputField.value = inputField.value.toUpperCase();
+            }
+
+            // Add event listener to the input field
+            var inputField = document.getElementById('fname');
+            inputField.addEventListener('blur', convertToUppercase);
+        });
